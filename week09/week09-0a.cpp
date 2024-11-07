7e31e19e8e501bea7572f91bd177f400e53b4329
Jeng-Sheng Yeh 葉正聖 <jsyeh.org@gmail.com>
2024-11-07 17:42:55 +0800

    week09

---
+++ week09/week09-0a.cpp
@@ -0,0 +1,12 @@
+// week09-0a.cpp SOIT107_ADVANCE_006
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char c;
+	while(cin >> c){
+		if(c!='2') cout<<c;
+	}
+	cout << "\n";
+}
---
+++ week09/week09-0b.cpp
@@ -0,0 +1,10 @@
+// week09-0b.cpp SOIT107_ADVANCE_006
+#include <stdio.h>
+#include <cstdio>
+int main()
+{
+	char c;
+	while( scanf("%c", &c) == 1 ){
+		if(c!='2') printf("%c", c);
+	}
+}
