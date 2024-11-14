c842a5414e9d3c6a9fe93ede0b63df3a33983eaf
Jeng-Sheng Yeh 葉正聖 <jsyeh.org@gmail.com>
2024-11-14 16:36:36 +0800

    week10

---
+++ week10/week10-3.cpp
@@ -0,0 +1,23 @@
+/// week10-3.cpp 進階的 class 裡面有建構函式、有資料、有方法
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Cat {
+public:
+    Cat(string _name) { ///小心底線(_) 前面不能寫 void
+        name = _name;
+    }
+    string name;  ///變數、資料
+    void print() {///函式、方法
+        cout << "I am a cat. My name is " << name  << ".\n";
+    }
+};
+
+int main()
+{
+    Cat cat1("小花"), cat2("小白");
+    cat1.print();
+    cat2.print();
+}
+
